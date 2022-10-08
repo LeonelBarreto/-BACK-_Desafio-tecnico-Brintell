af2217921da5e93c316ab903b77d332300af56de
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2022-10-08 16:06:59 -0300

    finalização da API

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -43,7 +43,57 @@ const listStudents = async (req, res) => {
     };
 };
 
+const updateStudent = async (req, res) => {
+    const { nome, cpf, sexo, email, telefone } = req.body;
+    const { id } = req.params;
+
+    try {
+        if ( !nome || !cpf || !sexo || !email || !telefone ) {
+            return res.status(400).json('Todos os campos são obrigatórios');
+        };
+        
+        const searchCpf = await knex("alunos").where("id", "!=", id).andWhere("cpf", cpf);
+
+        if (searchCpf.length > 0) {
+            return res.status(400).json("Já existe este cpf cadastrado por outro aluno.");
+        }
+
+        const updateData = await knex("alunos").update({ ...req.body }).where("id", id).returning("*");
+
+        if (updateData.length === 0) {
+            return res.status(400).json("Falha na atualização dos dados do aluno.");
+        }
+
+    return res.status(200).json("Atualização realizada com sucesso");
+    } catch (error) {
+        return res.status(400).json(error.message);
+    };
+};
+
+const deleteStudent = async (req, res) => {
+    const { id } = req.params;
+
+	try {
+		const studentFound = await knex('alunos').where("id", id);
+
+        if (studentFound.length === 0) {
+            return res.status(404).json("Aluno não encontrado");
+        };
+
+        const deleted = await knex('alunos').where("id", id).del();
+        if (deleted.length === 0) {
+            return res.status(400).json("Aluno não foi excluido");
+        };
+
+        return res.status(200).json("Aluno excluido com sucesso");
+	} catch (error) {
+        return res.status(400).json(error.message);
+	};
+};
+
 module.exports = {
     registerStudent,
-    listStudents
+    listStudents,
+    updateStudent,
+    deleteStudent
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { listStudents, registerStudent } = require('./controllers/students');
+const { listStudents, registerStudent, updateStudent, deleteStudent } = require('./controllers/students');
 
 const routes = express();
 
 routes.get('/dashboard', listStudents);
 routes.post('/dashboard', registerStudent);
+routes.put('/dashboard/:id', updateStudent);
+routes.delete('/dashboard/:id', deleteStudent);
 
 module.exports = routes;
